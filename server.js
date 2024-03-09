47a6b889ef97293a701c262bb97878a35872ecfc
kithonlimdotbn <162678985+kithonlimdotbn@users.noreply.github.com>
2024-03-09 12:21:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const port = 80
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World\n')
-  response.write('Username: admin)
-  response.write('Password: M6<hBx1rp8HF-c\£#d\n')
+  let name = request.query['name'];
+  if (name === null || name.trim() === "") {
+    response.write('Hello World\n')
+  } else {
+    response.write('Hello World! My name is ' + name + '\n')
+  }
+  response.write('Username: admin\n')
+  response.write('Password: M6<hBx1rp8HF-c£#d\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
